4bb14703009d08280e3e808ff6abe0d3fb54667a
PrinceL <PrinceL>
2020-09-26 20:17:26 +0200

    fixed 3 bugs

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -11,7 +11,7 @@ export default class Modal extends Component {
                 <ProductConsumer>
                     {(value) =>{
                         const {modalOpen,closeModal} = value;
-                        const {id, img, title, price} = value.modalProduct;
+                        const { img, title, price} = value.modalProduct;
 
                         if(!modalOpen)
                         {
